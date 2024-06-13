b0d8f1dd11c2fc34c25334398c5c5747d87e3925
Azhar rafi <azaharrafi17@gmail.com>
2024-06-13 05:43:08 +0530

    sixth

--- Server.js
+++ Server.js
@@ -4,12 +4,30 @@ import mongoose from "mongoose";
 import authRoute from "./routes/authRoute.js";
 import productRoute from "./routes/productRoute.js";
 import cors from "cors"
+import path from "path"
 //dotenv
 dotenv.config();
 
 
 const app=express();
 
+// deployment
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// deployment
 //middleware
 app.use(cors());
 app.use(express.json());
